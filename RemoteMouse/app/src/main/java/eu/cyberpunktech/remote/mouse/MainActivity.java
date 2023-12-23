fc255151c939a7abb3b9b84c8228587e759e5a62
Viktor Vaňo <viktorvano@gmail.com>
2023-12-23 18:18:48 +0100

    changed to accelerometer sensor

--- RemoteMouse/app/src/main/java/eu/cyberpunktech/remote/mouse/MainActivity.java
+++ RemoteMouse/app/src/main/java/eu/cyberpunktech/remote/mouse/MainActivity.java
@@ -12,11 +12,9 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,15 +23,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     private SensorManager sensorManager;
     private Sensor sensorMagneticField, sensorGravity;
-    private double xA, yA, zA, xM, yM, zM;
-    private Button buttonCalibrate, buttonInfinity, buttonDoubleClick, buttonLeft, buttonRight;
-    private TextView tvHeading, tvMagneticField, tvCalibration;
-    private ProgressBar progressBarCalibration;
+    private double xA, yA, zA;
+    private Button buttonDoubleClick, buttonLeft, buttonRight;
+    private TextView tvHeading;
     private long oldTime, currentTime;
-    private int calibrationReadings = 0;
-    private double oldPitch, oldRoll, integratedAngle;
-    private boolean calibrate = true;
-    private int headingReference, headingCompass;
     private Sender sender;
     private boolean doubleClick;
     private Switch switchActive;
@@ -48,27 +41,14 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-        headingReference = 0;
         doubleClick = false;
 
-        buttonCalibrate = findViewById(R.id.buttonCalibrate);
-        buttonInfinity = findViewById(R.id.buttonInfinity);
         buttonDoubleClick = findViewById(R.id.buttonDoubleClick);
         buttonLeft = findViewById(R.id.buttonLeft);
         buttonRight = findViewById(R.id.buttonRight);
         tvHeading = findViewById(R.id.tvHeading);
-        tvMagneticField = findViewById(R.id.tvMagneticField);
-        tvCalibration = findViewById(R.id.textViewCalibration);
-        progressBarCalibration = findViewById(R.id.progressBarCalibration);
         switchActive = findViewById(R.id.switchActive);
         oldTime = System.currentTimeMillis();
-
-        buttonCalibrate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                headingReference = headingCompass;
-            }
-        });
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         sensorGravity = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
@@ -95,6 +75,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         });
 
         sender = new Sender(serverIP, 8080, 200);
+        sender.start();
     }
 
     @Override
@@ -134,61 +115,27 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             zA = event.values[2];
         }
 
-        if(event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
-        {
-            xM = event.values[0];
-            yM = event.values[1];
-            zM = event.values[2];
-        }
-
-        double pitch = Math.atan2(-xA,Math.sqrt(yA*yA+zA*zA));
-        double roll = Math.atan2(yA,zA);
-
-        double cos_roll = Math.cos(roll);
-        double sin_roll = Math.sin(roll);
-
-        double cos_pitch = Math.cos(pitch);
-        double sin_pitch = Math.sin(pitch);
-
-        double yHeading = (yM*cos_roll) - ((float)zM*sin_roll);
-        double xHeading = (xM*cos_pitch) + ((float)yM*sin_roll*sin_pitch) + ((float)zM*cos_roll*sin_pitch);
-        int heading = (int) (Math.atan2(yHeading,xHeading)*radiansToDegrees) - 90;
-
-        while(heading>=360) heading -= 360;
-        while(heading<0) heading += 360;
-        heading = 360-heading;
-        if(heading==360) heading = 0;
-        headingCompass = heading;
-
-        heading = headingReference - heading;
-        while(heading >= 180 ){
-            heading -= 180;
-            heading = headingReference - heading;
-        }
-
-        tvHeading.setText("Pitch: " + (int)(roll*radiansToDegrees) + "°\nHeading: " + heading + "°");
-        tvMagneticField.setText((int)Math.sqrt(xM*xM + yM*yM + zM*zM)+"uT");
-
+        double roll = Math.atan2(-xA,zA);
+        double pitch = Math.atan2(yA,zA);
 
         currentTime = System.currentTimeMillis();
         if((currentTime - oldTime) > 100)
         {
-            tvHeading.setText("Pitch: " + (int)(roll*radiansToDegrees) + "°\nHeading: " + heading + "°");
-            tvMagneticField.setText((int)Math.sqrt(xM*xM + yM*yM + zM*zM)+"uT");
-            double normalizedPitch, normalizedYaw;
-            normalizedPitch = (roll*radiansToDegrees)/pitchRange;
+            tvHeading.setText("Pitch: " + (int)(pitch*radiansToDegrees) + "°\nRoll: " + (int)(roll*radiansToDegrees) + "°");
+            double normalizedPitch, normalizedRoll;
+            normalizedPitch = (pitch*radiansToDegrees)/pitchRange;
             if(normalizedPitch > 1.0)
                 normalizedPitch = 1.0;
             if(normalizedPitch < -1.0)
                 normalizedPitch = -1.0;
 
-            normalizedYaw = -heading/yawRange;
-            if(normalizedYaw > 1.0)
-                normalizedYaw = 1.0;
-            if(normalizedYaw < -1.0)
-                normalizedYaw = -1.0;
+            normalizedRoll = (roll*radiansToDegrees)/ rollRange;
+            if(normalizedRoll > 1.0)
+                normalizedRoll = 1.0;
+            if(normalizedRoll < -1.0)
+                normalizedRoll = -1.0;
             sender.setMessage("pitch:" + normalizedPitch +
-                            ",yaw:" + normalizedYaw +
+                            ",yaw:" + normalizedRoll +
                             ",LMB:" + buttonLeft.isPressed() +
                             ",DoubleClick:" + doubleClick +
                             ",RMB:" + buttonRight.isPressed() +
@@ -199,33 +146,5 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 doubleClick = false;
             }
         }
-
-        if((currentTime - oldTime) > 100 && calibrate)
-        {
-            if(calibrationReadings == 0)
-            {
-                oldPitch = Math.abs(pitch*57.29577951);
-                oldRoll = Math.abs(roll*57.29577951);
-            }else
-            {
-                integratedAngle += Math.abs(oldPitch-Math.abs(pitch)) + Math.abs(oldRoll-Math.abs(roll));
-                oldPitch = pitch;
-                oldRoll = roll;
-                progressBarCalibration.setProgress((int)(integratedAngle/2));
-                oldTime = currentTime;
-
-                if(progressBarCalibration.getProgress()==100)
-                {
-                    progressBarCalibration.setVisibility(View.GONE);
-                    tvCalibration.setVisibility(View.GONE);
-                    buttonInfinity.setVisibility(View.GONE);
-                    tvHeading.setVisibility(View.VISIBLE);
-                    calibrate = false;
-                    sender.start();
-                }
-            }
-            calibrationReadings++;
-        }
     }
-
 }
--- RemoteMouse/app/src/main/java/eu/cyberpunktech/remote/mouse/Variables.java
+++ RemoteMouse/app/src/main/java/eu/cyberpunktech/remote/mouse/Variables.java
@@ -3,6 +3,6 @@ package eu.cyberpunktech.remote.mouse;
 public class Variables {
     public static final double radiansToDegrees = 57.29577951;
     public static final double pitchRange = 25.0;
-    public static final double yawRange = 30.0;
+    public static final double rollRange = 30.0;
     public static String serverIP = "192.168.50.208";
 }
